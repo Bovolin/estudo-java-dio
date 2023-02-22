89108c9ad78fa18b08cb00f6078ba4053c27976d
Filipe <filipe.bovolin@gmail.com>
2023-02-22 17:13:28 -0300

    Tipos de Variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        //Variaveis
+        int idade = 10;
+        String nome = "Filipe";
+
+        //Constante = final
+        final double VALOR_DE_PI = 3.14;
     }
 }
