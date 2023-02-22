a9a2f0419e74d041433369c0403532b9ce7556ad
Filipe <filipe.bovolin@gmail.com>
2023-02-22 17:30:37 -0300

    Ternário

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -8,5 +8,15 @@ public class TiposVariaveis {
 
         //Constante = final
         final double VALOR_DE_PI = 3.14;
+
+        //ternario
+        String resultado;
+        if(idade == VALOR_DE_PI) resultado = "verdadeiro";
+        else resultado = "falso";
+        //ou
+        String resultado2 = idade == VALOR_DE_PI ? "verdadeiro" : "falso";
+        
+        System.out.println(resultado2);
+        System.out.println(resultado);
     }
 }
