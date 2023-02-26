2d6bb6c18724469b1842cbf5aefd385f8b583237
Filipe <filipe.bovolin@gmail.com>
2023-02-26 18:07:53 -0300

    Lista Circular

---
+++ ListaCircular/src/edu/filipe/listacircular/Main.java
@@ -0,0 +1,27 @@
+package edu.filipe.listacircular;
+
+public class Main {
+    
+    public static void main(String[] args){
+
+        ListaCircular<String> minhaLista = new ListaCircular<>();
+
+        minhaLista.add("c0");
+
+        System.out.println(minhaLista);
+
+        minhaLista.remove(0);
+
+        System.out.println(minhaLista);
+
+        minhaLista.add("c1");
+        minhaLista.add("c2");
+        minhaLista.add("c3");
+
+        System.out.println(minhaLista);
+
+        System.out.println(minhaLista.get(1));
+
+    }
+
+}
